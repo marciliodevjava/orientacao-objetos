97d984c74ab637f75fc1341dc10433a72b5e3245
m-arc <m-arc@192.168.1.8>
2022-04-22 23:56:13 -0300

    Atualização

--- src/oo/heranca/Jogo.java
+++ src/oo/heranca/Jogo.java
@@ -8,12 +8,17 @@ public class Jogo {
 		j1.x = 10;
 		j1.y = 10;
 
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
+		Jogador j2 = new Jogador();
+		j1.x = 10;
+		j1.y = 11;
 
-		System.out.println(j1.x);
-		System.out.println(j1.y);
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+
+		j1.atacar(j2);
+		j2.atacar(j1);
+
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
 	}
 }
