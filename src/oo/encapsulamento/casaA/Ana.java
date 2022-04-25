68ce352085e115d3ae0fbdb8d2a88bd34407a486
m-arc <m-arc@192.168.1.8>
2022-04-25 08:59:02 -0300

    Niveis de visibilidade

---
+++ src/oo/encapsulamento/casaA/Ana.java
@@ -0,0 +1,10 @@
+package oo.encapsulamento.casaA;
+
+public class Ana {
+
+	@SuppressWarnings("unused")
+	private String segredo = "...";// Privado
+	String facoDentroDeCasa = "...";// Default ou Pacote
+	protected String formaDeFalar = "...";// Protected
+	public String todosSabem = "...";// Publico
+}
