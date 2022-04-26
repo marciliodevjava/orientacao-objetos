c1963561f81a84b1fd8a06d9679b5a5f8a06cba7
m-arc <m-arc@192.168.1.8>
2022-04-26 08:00:36 -0300

    Atualização

--- src/oo/poliforfismo/Pessoa.java
+++ src/oo/poliforfismo/Pessoa.java
@@ -1,5 +1,21 @@
 package oo.poliforfismo;
 
 public class Pessoa {
+	
+	private double peso;
 
+	public Pessoa(double peso) {
+		setPeso(peso);
+	}
+	
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		if(this.peso >= 0) {
+		this.peso = peso;
+		}
+	}
+	
 }
