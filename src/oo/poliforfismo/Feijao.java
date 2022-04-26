0d5daaea019f4b4ac5d66b3720e425052b6225a7
m-arc <m-arc@192.168.1.8>
2022-04-26 07:58:17 -0300

    Atualização

--- src/oo/poliforfismo/Feijao.java
+++ src/oo/poliforfismo/Feijao.java
@@ -2,4 +2,19 @@ package oo.poliforfismo;
 
 public class Feijao {
 
+	private double peso;
+
+	public Feijao(double peso) {
+		setPeso(peso);
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		if (this.peso >= 0) {
+			this.peso = peso;
+		}
+	}
 }
