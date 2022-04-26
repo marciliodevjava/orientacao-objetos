1f85e800f38b9261ee2024a88719a4aece52c012
m-arc <m-arc@192.168.1.8>
2022-04-26 08:19:03 -0300

    Atualização

---
+++ src/oo/poliforfismo/Comida.java
@@ -0,0 +1,18 @@
+package oo.poliforfismo;
+
+public class Comida{
+
+	private double peso;
+
+	public Comida(double peso) {
+		this.peso = peso;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+}
--- src/oo/poliforfismo/Sorvete.java
+++ src/oo/poliforfismo/Sorvete.java
@@ -1,20 +1,8 @@
 package oo.poliforfismo;
 
-public class Sorvete {
+public class Sorvete extends Comida{
 
-	private double peso;
-	
 	public Sorvete(double peso) {
-		setPeso(peso);
-	}
-
-	public double getPeso() {
-		return peso;
-	}
-
-	public void setPeso(double peso) {
-		if (this.peso >= 0) {
-			this.peso = peso;
-		}
+		super(peso);
 	}
 }
