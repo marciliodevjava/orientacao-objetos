31fc9151951df68b4348e1ad43203cdecb613284
m-arc <m-arc@192.168.1.8>
2022-04-26 07:51:50 -0300

    Atualização

--- src/oo/encapsulamento/Pessoa.java
+++ src/oo/encapsulamento/Pessoa.java
@@ -2,21 +2,48 @@ package oo.encapsulamento;
 
 public class Pessoa {
 
+	private String nome;
 	private int idade;
+	private String sobreNome;
 
-	public Pessoa(int idade) {
-		alterarIdade(idade);
+	public Pessoa(String nome, String sobreNome, int idade) {
+		setSobreNome(sobreNome);
+		setNome(nome);
+		setIdade(idade);
 	}
 
-	public int lerIdade() {
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// Getters
+	public int getIdade() {
 		return this.idade;
 	}
 
-	public void alterarIdade(int novaIDade) {
+	// Settters
+	public void setIdade(int novaIDade) {
 		novaIDade = Math.abs(novaIDade);
 		if (novaIDade >= 0 && novaIDade <= 120) {
 			this.idade = novaIDade;
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "Olá eu sou o " + getNome() + " meu sobre nome é " + getSobreNome() + " minha idade é " + getIdade();
 	}
 
 }
--- src/oo/encapsulamento/PessoaTeste.java
+++ src/oo/encapsulamento/PessoaTeste.java
@@ -4,10 +4,12 @@ public class PessoaTeste {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(-30);
-		p1.alterarIdade(121);
+		Pessoa p1 = new Pessoa("Marcilio", "Silva", 45);
+		p1.setIdade(121);
+		p1.setNome("Marcilio");
 
-		System.out.println(p1.lerIdade());// Ler
+		System.out.println(p1.getNome());
+		System.out.println(p1);// Ler
 
 	}
 }
